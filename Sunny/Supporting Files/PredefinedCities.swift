1b69e55458eac2b12b4bbbbcb4f5cff29d43c402
Ironbatpug <molnarcsaba92@gmail.com>
2019-10-06 16:07:25 +0200

    remove the predefined cities from the view controller

---
+++ Sunny/Supporting Files/PredefinedCities.swift
@@ -0,0 +1,20 @@
+//
+//  PredefinedCities.swift
+//  Sunny
+//
+//  Created by Molnár Csaba on 2019. 10. 06..
+//  Copyright © 2019. Molnár Csaba. All rights reserved.
+//
+
+import Foundation
+
+var cities = [
+    Location(city: "Budapest", latitude: 47.4979, longitude: 19.0402),
+    Location(city: "New York", latitude: 40.730610, longitude: -73.935242),
+    Location(city: "Miskolc", latitude: 48.0964, longitude: 20.7624),
+    Location(city: "Debrecen", latitude: 47.4925, longitude: 19.0513),
+    Location(city: "London", latitude: 51.509865, longitude: -0.118092),
+    Location(city: "Paris", latitude: 48.8566, longitude: 2.3522),
+    Location(city: "Milan", latitude: 45.4655, longitude: 9.1865),
+    Location(city: "Tiszalúc", latitude: 48.0400768, longitude: 21.1042205)
+]
